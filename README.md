394c378e60983d257f5f43b8f99ce09e11aadb31
alchemyunited <mark.simchock@alchemyunited.com>
2014-12-14 14:40:49 -0500

    whoa. gotta true up to the parent class

--- README.md
+++ README.md
@@ -3,9 +3,13 @@
 
 ##### WordPress custom post types done The ezWay. 
 
-This is where your CPT magic happens. Once you set a handful of TODOs and you're up and running. 
+This is where your CPT magic happens. Once you set a handful of TODOs you're up and running. 
 
-Since you're going to edit this, make a copy (and be sure to rename it) of it to your "local" code (i.e., theme or plugin), fill in the TODOs, and that's it. This is all these is to creating CPTs The ezWay.
+Note: The boilerplate leans on:
+
+https://github.com/WPezClasses/class-wp-ezclasses-cpt-rpt-1
+
+Be sure to check the method: arguments_presets() prior to doing the TODOs of the boilerplate.
 
 =======================================================================================
 
@@ -16,11 +20,10 @@ Since you're going to edit this, make a copy (and be sure to rename it) of it to
 
 #### Overview
 
-- This class serves as the base. It takes care of some simple housekeeping. You lean on this but there's nothing for you to change here. 
+1) Make a copy of the boilerplate - giving it your own unique file + class name - into your theme or plugin:
 
-- The next layer up serves as a goto / boilerplate that configures - this is the important bit - a particular type of CPT. For example: https://github.com/WPezClasses/class-wp-ezclasses-cpt-rpt-1-public-exclude-from-search-1.  Again, you lean on this but there's nothing for you to change here. Chances are you'd create a new class for a new middle layer if necessary.
+2) Do the boilerplate's TODOs.
 
-- Finally, this boilerplate top layer (so to speak) is where you'll define a handful of setting that are unique to your CPT. For that, use this: https://github.com/WPezClasses/class-wp-ezclasses-cpt-rpt-1-boilerplate-1. This *is* where you'll be making any changes. Notice how few TODOs it takes to get a CPT up and running. 
+2.5) Don't forget to include / require it somewhere.
 
-
-Note: While it's possible to skip the middle layer (and move all particulars to the top), that's not going to help accomplish one of our primary goals: increase productivity by minimizing duplicate code / effort. 
+3) Your CPT is...Done! Yeah, that ez. 
--- class-wp-ezclasses-cpt-rpt-1-boilerplate-1.php
+++ class-wp-ezclasses-cpt-rpt-1-boilerplate-1.php
@@ -28,135 +28,77 @@ if ( ! defined('ABSPATH') ) {
 }
 
 if ( ! class_exists('TODO-CLASS_NAME' ) ){
-  class TODO-CLASS_NAME extends Class_WP_ezClasses_CPT_RPT_1_Public_Exclude_From_Search_1-TODO {
+  class TODO-CLASS_NAME extends Class_WP_ezClasses_CPT_RPT_1 {
 
-    protected $_str_post_type;  
-
-		protected function __construct() {	
-			parent::__construct();
-		}
-		
-		/*
-		 * * * IMPORTANT * * * 
-		 *
-		 * When you do the: TODO-CLASS_NAME::ez_new('my_post_type_name');
-		 *
-		 * Be sure to pass your post type name. All the other parms that matter are below. This gives you
-		 * a "last minute" opportunity to name the CPT.
-		 */
-		public function ez__construct( $str_post_type = '' ) {
-		
-		  $this->_str_post_type = $str_post_type;
-		
-		  add_action( 'init', array($this, 'cpt_do'), 50);
-		  
-		}
-		
-		protected function this_cpt_do( $str_cpt_name = '' ){
-		
-		  $str_post_type = $this->_str_post_type;
-		  if ( empty($str_post_type) ){
-		    $str_post_type = $this->this_post_type();
-		  }
-		
-		  $arr_args = array(
-			  
-			'post_type'		=> $str_post_type,
-			'labels'		=> $this->this_post_type_labels(),
-			'supports'		=> $this->this_post_type_supports(),
-			'arguments'		=> $this->this_post_type_arguments(),
-			'capabilities'	=> $this->capabilities_settings(),
-			);
-		  return  $arr_args;
-		
-		}
-		
-		/**
-		 * TODO or you can pass the name is an the arg of ::ez_new()
-		 */
-		protected this_post_type(){
-		
-		  return substr('TODO_' . __CLASS__, 0, 19);
-		
-		}
+	/**
+	 * This is where your magic happens. The idea here is to (re) define as little as possible. 
+	 *
+	 * It is recommended you use this  boilerplate: >>>>>>>>>>>>>>>> TODO <<<<<<<<<<<<<
+	 *
+	 * This method remains simply as an example. 
+	 *
+	 * Ref: http://codex.wordpress.org/Function_Reference/register_post_type
+	 */			
+    public function cpt_todo(){
+	  
+	  // $this->_str_action = 'init';
+	  // $this->_int_priority = 50;
+	  
+	  
+	  $str_args_labels_name_plural = 'TODO';		// just saves a couple key strokes. 
+	  
+	  $arr_labels = array(
+	    'name'					=> 'TODO',			// typically plural
+		'singular_name' 		=> 'TODO',
+		'search_items' 			=> 'Search ' . $str_args_labels_name_plural,
+		'not_found' 			=> 'No ' . $str_args_labels_name_plural . ' found',
+		'not_found_in_trash'	=> 'No ' . $str_args_labels_name_plural . ' found in trash',
+		'menu_name'				=> 'TODO'
+		);
 		
 		
-		protected function this_post_type_labels(){
+	  $arr_supports = array(
+	    'title'				=> true,
+		'editor'			=> true,
+		'author'			=> true,
+		'thumbnail' 		=> true,			// featured image, current theme must also support post-thumbnails
+		'excerpt'			=> true,		
+		'trackbacks'		=> false,
+		'custom-fields'		=> false,			
+		'comments'			=> false,			// also will see comment count balloon on edit screen
+		'revisions'			=> false,			// will store revisions
+		'page-attributes'	=> false, 			// menu order, hierarchical must be true to show Parent option
+		'post-formats' 		=> false,			// add post formats
+		);
 		
-			$str_labels_plural_name = 'TODO';
+	  $arr_capabilities = array();
+	  
+	  
+	  $this->_arr_args_preset_key = 'TODO'; 	// the key of the preset (that can override the arguments_defaults but before your custom settings)
 		
-			$arr_register_post_type_labels	= array(
+	  $arr_cpt_todo = array(
+	  
+	    'post_type'		=> 'TODO',
 	
-												'name'					=> 'TODO',
-												'singular_name' 		=> 'TODO',
-												'search_items' 			=> 'Search ' . $str_labels_plural_name,
-												'not_found' 			=> 'No ' . $str_labels_plural_name . ' found',
-												'not_found_in_trash'	=> 'No ' . $str_labels_plural_name . ' found in trash',
-												'menu_name'				=> 'TODO'
-											);	
-												
-			return $arr_register_post_type_labels;
-		}
+		'arguments'		=> array(
 		
-		/*
-		 * Configure these or just rely on the parent(s). 
-		 */
-		protected function this_post_type_supports(){
-		
-		  $arr_register_post_type_supports = array();
+		  'description'		=> 'TODO',
+		  'rewrite' 		=> array(
+		    'slug' => 'TODO',
+			),
+		  'menu_position' 	=> 30,	// TODO
+		  'menu_icon' 		=> NULL,
 		  
-		  /*
-		  $arr_register_post_type_supports = array(
-		  
-		    'title'				=> true,
-			'editor'			=> true,
-			'author'			=> true,
-			'thumbnail' 		=> true,			// (featured image, current theme must also support post-thumbnails)
-			'excerpt'			=> true,		
-			'trackbacks'		=> false,
-			'custom-fields'		=> false,			
-			'comments'			=> false,			// (also will see comment count balloon on edit screen)
-			'revisions'			=> false,			// (will store revisions)
-			'page-attributes'	=> false, 			//(menu order, hierarchical must be true to show Parent option)
-			'post-formats' 		=> false,			// add post formats,
-		  );
-		  */
-		  
-		  return $arr_register_post_type_supports;
-		}
-		
-		
-		
-		/*
-		 * Note: You can add any other args here. This is the minimum viable subset that needs tending to.
-		 */
-		protected function this_post_type_arguments(){
-		
-		  $arr_register_post_type_arguments = array(
-		  
-		    'description'		=> 'TODO',
-			'rewrite' 			=> array('slug' => 'TODO'),
-			'menu_position' 	=> 30,	
-			'menu_icon' 		=> NULL,
-		  );
-		  
-		  return $arr_register_post_type_arguments;
-		}
-					
-		/*
-		 * capabilites are unique to a given project and should be manually configured.
-		 *
-		 * that said, it's not impossible that you'll design your own parent class with a typical fixed set of caps
-		 * and then override as necessary.
-		 */
-		public function capabilities_settings(){
-		
-			$arr_capabilities = = array();
-			
-			return $arr_capabilities;
-		}
-		
-	}
+		  'labels'			=> $arr_labels,
+		  'supports'		=> $arr_supports,
+		  'capabilities'	=> $arr_capabilities, 
+		  			
+		  ),
+	    );
+	  
+	  return $arr_cpt_todo;	
+    }  
+  }
 }
 
 // Don't worry about the add_action('init',...) that's taken care of as well :)
